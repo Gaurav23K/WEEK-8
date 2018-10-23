1cfdee981fc4409b5e61ed7fe6d836f6ebde63b3
Gaurav23K <42205976+Gaurav23K@users.noreply.github.com>
2018-10-23 20:25:44 +0530

    Add files via upload

---
+++ w8q1.cpp
@@ -0,0 +1,22 @@
+//this is the library file
+#include <iostream>
+using namespace std;
+//to find the sum of the elements
+void func(int arr[],int i,int sum)
+{
+        while(i<5){
+        sum=sum+arr[i];
+        i++;
+        	  }
+
+cout<<sum<<endl;
+}
+//the main function
+int main()
+{
+   int arr[5]={23,67,41,3,14};
+   func(arr,0,0);
+//GETTING TRUE VALUES
+   return 0;
+//DONE!!!
+}
---
+++ w8q3.cpp
@@ -0,0 +1,50 @@
+//this is the library file
+#include <iostream>
+using namespace std;
+
+int no(int arr[])
+{
+for(int i=0;i<7;i++)
+	{
+	for(int j=i;j<7;j++)
+	{
+	if(arr[i]>arr[j])
+		{
+		int a=arr[i];
+		arr[i]=arr[j];
+		arr[j]=a;
+		}
+	}
+	}
+}
+int no2(int arr[])
+{
+
+for(int i=0;i<7;i++)
+	{
+	for(int j=i;j<7;j++)
+	{
+	if(arr[i]<arr[j])
+		{
+		int a=arr[i];
+		arr[i]=arr[j];
+		arr[j]=a;
+		}
+	}
+	}
+}
+int main()
+{int n;int m;
+int arr[7]={7,65,34,27,28,58,99};
+cout<<"which element you would like to be printed in the order of largest to smallest"<<endl;
+cin>>n;
+no(arr);
+cout<<"The element in the order of smallest to largest"<<endl;
+cin>>m;
+no2(arr);
+cout<<arr[n-1]<<endl;
+ cout<<arr[m-1]<<endl;
+//GETTING TRUE VALUE
+return 0;
+//DONE!!!
+}
